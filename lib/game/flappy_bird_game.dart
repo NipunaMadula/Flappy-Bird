a1d8d473f4cf5f3d9dfda3e33ad59faec4179d34
Nipuna Madula <nipunamadula2001@gmail.com>
2025-04-17 13:23:55 +0530

    Add sky background rendering

--- lib/game/flappy_bird_game.dart
+++ lib/game/flappy_bird_game.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import "package:flame/game.dart";
 import 'package:flame/input.dart';
 
@@ -5,6 +6,15 @@ class FlappyBirdGame extends FlameGame with TapDetector {
   @override
   Future<void> onLoad() async {
 
+  }
+
+  @override
+  void render(Canvas canvas) {
+    canvas.drawRect(
+      Rect.fromLTWH(0, 0, size.x, size.y),
+      Paint()..color = const Color(0xFF87CEEB), // Sky blue
+    );
+    super.render(canvas);
   }
   
   @override

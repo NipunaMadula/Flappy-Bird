6b68723a730f27eea3445c7907ac69de2e53ddb8
Nipuna Madula <nipunamadula2001@gmail.com>
2025-05-02 15:36:48 +0530

    Connect bird to game class

--- lib/game/flappy_bird_game.dart
+++ lib/game/flappy_bird_game.dart
@@ -1,16 +1,21 @@
 import 'package:flutter/material.dart';
 import "package:flame/game.dart";
 import 'package:flame/input.dart';
+import 'package:flappy_bird/game/bird.dart';
+
 
 class FlappyBirdGame extends FlameGame with TapDetector {
+
+  final Bird bird = Bird();
+
   @override
   Future<void> onLoad() async {
-
+    bird.y = size.y / 2;
   }
 
   @override
   void update(double dt) {
-    
+    bird.update(dt);
     super.update(dt);
   }
 
@@ -20,11 +25,18 @@ class FlappyBirdGame extends FlameGame with TapDetector {
       Rect.fromLTWH(0, 0, size.x, size.y),
       Paint()..color = const Color(0xFF87CEEB), // Sky blue
     );
+
+    canvas.drawCircle(
+      Offset(size.x / 3, bird.y),
+      20,
+      Paint()..color = Colors.red,
+    );
+
     super.render(canvas);
   }
   
   @override
   void onTap() {
-    print('Screen tapped!');
+    bird.jump();
   }
 }

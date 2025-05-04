53be401fbe6065902825e1c6eff127da1abe53e4
Nipuna Madula <nipunamadula2001@gmail.com>
2025-05-04 23:56:00 +0530

    Connect pipe system to game

--- lib/game/flappy_bird_game.dart
+++ lib/game/flappy_bird_game.dart
@@ -2,11 +2,13 @@ import 'package:flutter/material.dart';
 import "package:flame/game.dart";
 import 'package:flame/input.dart';
 import 'package:flappy_bird/game/bird.dart';
+import 'package:flappy_bird/game/pipe_system.dart';
 
 
 class FlappyBirdGame extends FlameGame with TapDetector {
 
   final Bird bird = Bird();
+  final PipeSystem pipeSystem = PipeSystem();
 
   @override
   Future<void> onLoad() async {
@@ -16,6 +18,7 @@ class FlappyBirdGame extends FlameGame with TapDetector {
   @override
   void update(double dt) {
     bird.update(dt);
+    pipeSystem.update(dt);
     super.update(dt);
   }
 
@@ -31,6 +34,19 @@ class FlappyBirdGame extends FlameGame with TapDetector {
       20,
       Paint()..color = Colors.red,
     );
+
+    final Paint pipePaint = Paint()..color = Colors.green;
+    for (double x in pipeSystem.pipes) {
+      canvas.drawRect(
+        Rect.fromLTWH(x, 0, 50, size.y/2 - pipeSystem.gap/2),
+        pipePaint,
+      );
+
+      canvas.drawRect(
+        Rect.fromLTWH(x, size.y/2 + pipeSystem.gap/2, 50, size.y/2 - pipeSystem.gap/2),
+        pipePaint,
+      );
+    }
 
     super.render(canvas);
   }

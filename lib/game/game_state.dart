b6e216d40944d5e5cb0e9c29a4d4db28fac1aeb4
Nipuna Madula <nipunamadula2001@gmail.com>
2025-05-12 00:39:46 +0530

    Add game manager class

--- lib/game/game_state.dart
+++ lib/game/game_state.dart
@@ -3,3 +3,13 @@ enum GameState {
   playing,
   gameOver,
 }
+
+class GameManager {
+  GameState state = GameState.ready;
+  int score = 0;
+
+  void reset() {
+    state = GameState.ready;
+    score = 0;
+  }
+}
